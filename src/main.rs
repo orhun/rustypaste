df0d22dcb38a55e3b4a38eae41f11361508ed359
orhun <orhun@archlinux.org>
2021-07-20 01:35:52 +0300

    feat(server): add basic HTTP server implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate log;
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/")
+            .route(web::get().to(index))
+            .route(web::head().to(|| HttpResponse::MethodNotAllowed())),
+    );
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    HttpServer::new(|| App::new().wrap(Logger::default()).configure(config))
+        .bind("127.0.0.1:8000")?
+        .workers(4)
+        .run()
+        .await
 }
