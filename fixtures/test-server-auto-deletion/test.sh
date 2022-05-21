d262a3d2972145504740dfd5d58e14d9cd6f94cb
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-05-21 18:23:21 +0300

    test(fixtures): add fixture test for auto deletion

---
+++ fixtures/test-server-auto-deletion/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+content="test content"
+
+setup() {
+  echo "$content" > file
+}
+
+run_test() {
+  first_file_url=$(curl -s -F "file=@file" -H "expire:1s" localhost:8000)
+  second_file_url=$(curl -s -F "file=@file" -H "expire:4s" localhost:8000)
+  test "$content" = "$(curl -s $first_file_url)"
+  test "$content" = "$(curl -s $second_file_url)"
+  sleep 3s
+  test "file is not found or expired :(" = "$(curl -s $first_file_url)"
+  test "$content" = "$(curl -s $second_file_url)"
+  sleep 1s
+  test "file is not found or expired :(" = "$(curl -s $second_file_url)"
+}
+
+teardown() {
+  rm file
+  rm -r upload
+}
