c06fff92aca43ca0a6ba12106bcae938e9c94947
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-05-21 18:47:41 +0300

    test(fixtures): add fixture test for MIME blacklist

---
+++ fixtures/test-server-mime-blacklist/test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+content="test data"
+
+setup() {
+  echo "<html></html>" > file.html
+  echo '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>' > file.xml
+  echo "$content" > file.txt
+}
+
+run_test() {
+  test "this file type is not permitted" = "$(curl -s -F "file=@file.html" localhost:8000)"
+  test "this file type is not permitted" = "$(curl -s -F "file=@file.xml" localhost:8000)"
+  file_url=$(curl -s -F "file=@file.txt" localhost:8000)
+  test "$content" = "$(curl -s $file_url)"
+}
+
+teardown() {
+  rm file*
+  rm -r upload
+}
