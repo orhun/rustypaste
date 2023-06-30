e5691db773013a8f493e97450c25700b45c8ee51
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2023-07-01 01:07:12 +0300

    style(format): fix the indentation for random suffix fixture

--- fixtures/test-random-suffix/test.sh
+++ fixtures/test-random-suffix/test.sh
@@ -3,27 +3,27 @@
 content="test data"
 
 setup() {
-	echo "$content" >file
+  echo "$content" >file
 }
 
 run_test() {
-	first_file_url=$(curl -s -F "file=@file" localhost:8000)
-	test "$first_file_url" != "http://localhost:8000/file.txt"
-	test "$content" = "$(curl -s $first_file_url)"
+  first_file_url=$(curl -s -F "file=@file" localhost:8000)
+  test "$first_file_url" != "http://localhost:8000/file.txt"
+  test "$content" = "$(curl -s $first_file_url)"
 
-	second_file_url=$(curl -s -F "file=@file" localhost:8000)
-	test "$first_file_url" != "http://localhost:8000/file.txt"
-	test "$content" = "$(curl -s $first_file_url)"
+  second_file_url=$(curl -s -F "file=@file" localhost:8000)
+  test "$first_file_url" != "http://localhost:8000/file.txt"
+  test "$content" = "$(curl -s $first_file_url)"
 
-	test "$first_file_url" != "$second_file_url"
+  test "$first_file_url" != "$second_file_url"
 
-	test "$(cat upload/${first_file_url/http:\/\/localhost:8000\//})" \
-		= "$(cat upload/${second_file_url/http:\/\/localhost:8000\//})"
+  test "$(cat upload/${first_file_url/http:\/\/localhost:8000\//})" \
+    = "$(cat upload/${second_file_url/http:\/\/localhost:8000\//})"
 
-	[[ $(find upload/ -name "file.*.txt" -print -quit 2>/dev/null) ]] || exit 1
+  [[ $(find upload/ -name "file.*.txt" -print -quit 2>/dev/null) ]] || exit 1
 }
 
 teardown() {
-	rm file
-	rm -r upload
+  rm file
+  rm -r upload
 }
