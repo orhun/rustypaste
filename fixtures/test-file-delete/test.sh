43cd807ed9339897a80e26d0a751b06b2d904a9e
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2023-09-04 04:59:46 -0400

    refactor(server): change response to 'file deleted'

--- fixtures/test-file-delete/test.sh
+++ fixtures/test-file-delete/test.sh
@@ -9,7 +9,7 @@ setup() {
 run_test() {
   file_url=$(curl -s -F "file=@file" localhost:8000)
   test "$file_url" = "http://localhost:8000/file.txt"
-  test "the file is deleted" = "$(curl -s -H "Authorization: may_the_force_be_with_you" -X DELETE http://localhost:8000/file.txt)"
+  test "file deleted" = "$(curl -s -H "Authorization: may_the_force_be_with_you" -X DELETE http://localhost:8000/file.txt)"
   test "file is not found or expired :(" = "$(curl -s -H "Authorization: may_the_force_be_with_you" -X DELETE http://localhost:8000/file.txt)"
 }
 
