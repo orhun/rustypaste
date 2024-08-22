4812a252eef0ee144c01b90ef8cf8bb9c5bb851c
Nydragon <contact@ccnlc.eu>
2024-08-22 22:28:53 +0200

    test(fixtures): add the ability to set env variables in the scope of a single fixture
    Define the custom_env function in your fixture to set environment variables.

--- fixtures/README.md
+++ fixtures/README.md
@@ -7,7 +7,7 @@ This directory contains the [test fixtures](https://en.wikipedia.org/wiki/Test_f
 1. Build the project in debug mode: `cargo build`
 2. Execute the runner script in this directory: `./test-fixtures.sh`
 
-On `macOS` you need to have [coreutils](https://www.gnu.org/software/coreutils/) installed to run the script. 
+On `macOS` you need to have [coreutils](https://www.gnu.org/software/coreutils/) installed to run the script.
 The simplest way is to install it via [Homebrew](https://brew.sh/): `brew install coreutils`
 
 ### Adding new fixtures
@@ -27,6 +27,11 @@ test-file-upload/
 
 ```sh
 #!/usr/bin/env bash
+
+# Optional
+custom_env() {
+  # setting environment variables such as AUTH_TOKEN or AUTH_FILE
+}
 
 setup() {
   # preparation
--- fixtures/test-fixtures.sh
+++ fixtures/test-fixtures.sh
@@ -9,11 +9,13 @@ NC='\033[0m'
 run_fixture() {
   cd "$FIXTURE_DIR/$1" || exit 1
   source "test.sh"
+  type custom_env &>/dev/null && custom_env
   NO_COLOR=1 CONFIG=config.toml "$PROJECT_DIR/target/debug/rustypaste" &
   SERVER_PID=$!
   trap 'kill -9 "$SERVER_PID" && wait "$SERVER_PID" 2> /dev/null' RETURN
   sleep 1
-  ( set -e;
+  (
+    set -e
     setup
     run_test
     teardown
@@ -24,7 +26,9 @@ run_fixture() {
 
 main() {
   find * -maxdepth 0 -type d -print0 | while IFS= read -r -d '' fixture; do
-    run_fixture "$fixture"
+    # Since we are creating a subshell, all environment variables created by custom_env will be lost
+    # Return code is preserved
+    (run_fixture "$fixture")
     exit_status=$?
     if [ "$exit_status" -eq 0 ]; then
       echo -e "[${GREEN}ok${NC}] $fixture"
