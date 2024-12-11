84305fb37df2879c8ba713ffc6b48c1b7dfffef0
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-12-11 14:19:37 +0300

    refactor(server): vendor get_size function from fx_extra

--- src/paste.rs
+++ src/paste.rs
@@ -114,14 +114,11 @@ impl Paste {
         }
 
         if let Some(max_dir_size) = config.server.max_upload_dir_size {
-            // The unwrap here should be fine as the max value of u64 will be within the limits
             let file_size = u64::try_from(self.data.len()).unwrap_or_default();
             let upload_dir = self.type_.get_path(&config.server.upload_path)?;
-            let current_size_of_upload_dir = fs_extra::dir::get_size(upload_dir)
+            let current_size_of_upload_dir = util::get_dir_size(&upload_dir)
                 .map_err(|_| error::ErrorInternalServerError("Internal server error occured"))?;
-
             let expected_size_of_upload_dir = current_size_of_upload_dir.add(file_size);
-
             if expected_size_of_upload_dir > max_dir_size {
                 return Err(error::ErrorInsufficientStorage(
                     "upload directory size limit exceeded",
