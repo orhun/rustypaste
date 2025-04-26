20821b287dd06a93848f70e0ce99d0ef946ab080
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2025-04-26 17:39:37 -0400

    fix(fixtures): adjust test-server-upload-dir-limit
    The test-server-upload-dir-limit failed for multiple reasons:
    
    - truncate on macOS does not understand KB
    - curl returns an exit code 23, if you try to output a binary file to
      the terminal
    - bash prints a warning, if it encounters a null byte (this does not
      fail the test, but is still annoying)

--- fixtures/test-server-upload-dir-limit/test.sh
+++ fixtures/test-server-upload-dir-limit/test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 setup() {
-  truncate -s 9KB bigfile1 bigfile2 bigfile3
+  truncate -s 9000 bigfile1 bigfile2 bigfile3
 }
 
 run_test() {
   result=$(curl -s -F "file=@bigfile1" localhost:8000)
   result=$(curl -s -F "file=@bigfile2" localhost:8000)
-  curl -s "$result"
+  content=$(curl -s "$result" &>/dev/null)
 
   result=$(curl -s -F "file=@bigfile3" localhost:8000)
   test "upload directory size limit exceeded" = "$result"
 }
 
 teardown() {
-  rm bigfile* 
+  rm bigfile*
   rm -r upload
 }
